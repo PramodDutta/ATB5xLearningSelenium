f32a2543425e1a49f54e1dd99eaeeab269bf52ac
Promode <thetestingacademy@gmail.com>
2024-03-16 11:28:08 +0530

    15 March 2024

---
+++ src/test/java/com/thetestingacademy/selenium16032024/Selenium11.java
@@ -0,0 +1,67 @@
+package com.thetestingacademy.selenium16032024;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Selenium11 {
+    @Test(groups = "QA")
+    @Description("Verify the current URl, title of VWO app")
+    public void testVWOLogin(){
+        WebDriver driver = new EdgeDriver();
+        // 1. Open the URL .app.vwo.com/#/login](https://app.vwo.com/#/login)
+        driver.get("https://app.vwo.com");
+        System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Login - VWO");
+        Assert.assertEquals(driver.getCurrentUrl(),"https://app.vwo.com/#/login");
+
+//        **Project #1 - TC ( Negaative) - Invalid username, pass - Error message**
+//
+//        1. Open the URL .app.vwo.com/#/login](https://app.vwo.com/#/login)
+//        2. **Find the Email id** and enter the email as admin@admin.com
+//        3. **Find the pass inputbox** and enter passwrod as admin.
+//        4. Find and Click on the submit button
+//        5. Verify that the error message is shown "Your email, password, IP address or location did not match"
+//
+
+        // 2. **Find the Email id** and enter the email as admin@admin.com
+        //  id, className, name, css Selector, xpath
+        //  LinkText and PartialText which are onlu for <a>
+        // <input
+        // type="email"
+        // class="text-input W(100%)"
+        // name="username"
+        // id="login-username"
+        // data-qa="hocewoqisi">
+        WebElement emailInputBox =  driver.findElement(By.id("login-username"));
+        emailInputBox.sendKeys("admin@admin.com");
+
+        WebElement passwordInputBox = driver.findElement(By.name("password"));
+        passwordInputBox.sendKeys("admin");
+
+        driver.findElement(By.id("js-login-btn")).click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement error_msg = driver.findElement(By.className("notification-box-description"));
+        String error_msg_text  = error_msg.getText();
+
+        Assert.assertEquals(error_msg_text,"Your email, password, IP address or location did not match");
+
+
+        driver.quit(); // Stop the session and edge browser which is opened.
+
+
+
+    }
+
+
+}
---
+++ src/test/java/com/thetestingacademy/selenium16032024/Selenium12.java
@@ -0,0 +1,7 @@
+package com.thetestingacademy.selenium16032024;
+
+public class Selenium12 {
+    // LinkText & Partial Text Text
+    // Css Selctors
+    // Xpath
+}
